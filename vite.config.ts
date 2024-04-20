efb7a603b905675d2cfcf045b53f548c3fdd495d
Pedro Silva <pedro.silva-dev@hotmail.com>
2024-04-20 00:08:05 -0300

    feat: init pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,56 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const confgPWA: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  workbox: {
+    clientsClaim: true,
+    skipWaiting: true,
+  },
+  manifest: {
+    name: "Daily",
+    short_name: "daily",
+    description: "App de gestão para todas as áreas da sua vida",
+    icons: [
+      {
+        src: "/icons/180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/icons/192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/icons/512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+
+      {
+        src: "/icons/512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    scope: "/",
+    start_url: "/",
+    display: "standalone",
+    orientation: "portrait",
+    theme_color: "#FFFF",
+    background_color: "#0a0a0a",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(confgPWA)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
